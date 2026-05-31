788f1d11f156b47b053b8ca83f1aee7dc67cccd0
V3KTOR <thevector1994@gmail.com>
2026-05-31 13:07:07 -0500

    primer commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,12 @@
+/**
+ * Esta función me permite tomar una carta
+ * @param {Array>String>} deck es un arreglo de string que representa el deck de cartas
+ * @returns {String} retorna una carta del deck
+ */
+export const pedirCarta = (deck) => {
+    if (!deck || deck.length === 0) {
+        throw "No hay cartas en el deck";
+    }
+    const carta = deck.pop();
+  return carta;
+};
